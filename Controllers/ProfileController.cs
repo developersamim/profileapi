20be2b46c422cd7bb3a35e15c3a8d32efa8ee2ae
Samim Ahmed <developersamim@gmail.com>
2018-11-06 21:46:49 +1100

    only two methods

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -36,13 +36,6 @@ namespace ProfileApi.Controllers
             
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
         // POST api/values
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] ProfileDTO profileDTO)
@@ -53,18 +46,6 @@ namespace ProfileApi.Controllers
             var client = _httpClientFactory.CreateClient("ProfileApiClient");
             var result = await client.PostAsJsonAsync("", profileDTO);
             return Ok(await result.Content.ReadAsStringAsync());
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
