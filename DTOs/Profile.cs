6ae49aeb9a79287726fd04ce051ba42b39215b55
Samim Ahmed <developersamim@gmail.com>
2018-11-06 18:10:29 +1100

    added model

---
+++ DTOs/Profile.cs
@@ -0,0 +1,16 @@
+using System;
+using System.IO;
+
+namespace ProfileApi.DTOs
+{
+    public class Profile{
+        public int userId{get;set;}
+        public string email{get;set;}
+        public string firstName{get;set;}
+        public string lastName{get;set;}
+        public string displayName{get;set;}
+        public string description{get;set;}
+        public string department{get;set;}
+        public string team{get;set;}
+    }
+}
---
+++ Services/ProfileService.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+
+namespace ProfileApi.Services
+{
+    public class ProfileService
+    {
+        public void Add()
+        {
+            
+        }
+    }
+}
